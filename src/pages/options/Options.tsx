f8417d0d70f7452c8e1fa05cfaf92a9cbe1362e5
Raphael Sanchez <sanchezraphael0@gmail.com>
2024-11-06 07:46:20 -0500

    style: options page

--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -1,10 +1,10 @@
-import { Card, CardTitle } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Options() {
   return (
-    <div className="w-screen h-screen">
-      <Card>
-        <CardTitle>Options</CardTitle>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <Card className="w-1/3 h-2/4">
+        <CardHeader>Canvas URL</CardHeader>
       </Card>
     </div>
   );
