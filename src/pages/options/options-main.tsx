08e812184b52016b1fa8a9c6de222524d102e38e
Raphael Sanchez <sanchezraphael0@gmail.com>
2024-11-06 07:05:01 -0500

    options supports tailwind

--- src/pages/options/options-main.tsx
+++ src/pages/options/options-main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Options from "./Options";
+import "@/globals.css";
 
 createRoot(document.getElementById("options-root")!).render(
   <StrictMode>
